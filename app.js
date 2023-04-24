d09dfa897814b9afaa67128aa03d875fbb4f6f6c
Saurabh Choudhary <90515478+SaurabhGurjar@users.noreply.github.com>
2023-04-25 00:46:40 +0530

    Change width and height of pixel according to grid size

--- app.js
+++ app.js
@@ -1,32 +1,37 @@
+const WIDTH = 480;
+const MAXPIXELS = 100;
+
 const canvas = document.querySelector('.canvas');
-const pixelDensity = document.querySelector('.user-input');
+const setSize = document.querySelector('.user-input');
 
 
-let pixels;
-
+// Get the Number of pixels from the user
 function getPixels() {
+    let pixelSize, pixels, gridSize;
     do {
-        pixels = prompt('Enter the Number of Pixels!', 'Value must be below 100');
-    } while (pixels > 100);
+        pixels = prompt('Enter the Number of Pixels(2 for 2 X 2 grid)!', `Value must be below ${MAXPIXELS}`);
+    } while (pixels > MAXPIXELS);
     pixels = parseInt(pixels);
-    let gridSize = pixels * pixels;
+
+    pixelSize = Math.floor(WIDTH / pixels);
+    gridSize = pixels * pixels;
 
     if (canvas.hasChildNodes()) {
         clearCanvas();
     }
 
-    changePixelDensity(gridSize);
+    changePixelDensity(gridSize, pixelSize);
 }
 
-pixelDensity.addEventListener('click', getPixels);
 
-
-function changePixelDensity(gridSize) {
+// Create grid on canvas
+function changePixelDensity(gridSize, pixelSize) {
     for (let i = 0; i < gridSize; i++) {
         const newDiv = document.createElement('div');
         newDiv.className = 'pixel';
         newDiv.setAttribute('id', `${i + 1}`);
         newDiv.textContent = `${i + 1}`;
+        newDiv.setAttribute('style', `width: ${pixelSize}px; height: ${pixelSize}px`);
         canvas.appendChild(newDiv);
     }
 }
@@ -39,3 +44,5 @@ function clearCanvas() {
     canvas.removeChild(canvas.childNodes[(canvas.childNodes.length - 1)]);
     clearCanvas();
 }
+
+setSize.addEventListener('click', getPixels);

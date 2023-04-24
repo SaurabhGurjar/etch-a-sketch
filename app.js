ab94430e8a362b62b9fd5ee53b08184cb3b9b7fb
Saurabh Choudhary <90515478+SaurabhGurjar@users.noreply.github.com>
2023-04-24 23:19:12 +0530

    Add grid to canvas

--- app.js
+++ app.js
@@ -9,5 +9,21 @@ function getPixels() {
         pixels = prompt('Enter the Number of Pixels!', 'Value must be below 100');
     } while (pixels > 100);
     pixels = parseInt(pixels);
+    let gridSize = pixels * pixels;
+
+    changePixelDensity(gridSize);
+}
+
+pixelDensity.addEventListener('click', getPixels);
+
+
+function changePixelDensity(gridSize) {
+    for (let i = 0; i < gridSize; i++) {
+        const newDiv = document.createElement('div');
+        newDiv.className = 'pixel';
+        newDiv.setAttribute('id', `${i + 1}`);
+        newDiv.textContent = `${i + 1}`;
+        canvas.appendChild(newDiv);
+    }    
 }
 

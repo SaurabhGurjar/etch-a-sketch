26a00772f8b6972bd3fa8de3b2174833463789fc
Saurabh Choudhary <90515478+SaurabhGurjar@users.noreply.github.com>
2023-04-24 23:16:20 +0530

    Get user input

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
-const  PIXELS = 256;
-
 const canvas = document.querySelector('.canvas');
+const pixelDensity = document.querySelector('.user-input');
 
-for (let i = 0; i < PIXELS; i++) {
-    const newDiv = document.createElement('div');
-    newDiv.className = 'pixel';
-    newDiv.setAttribute('id', `${i + 1}`);
-    newDiv.textContent = `${i + 1}`;
-    canvas.appendChild(newDiv);
+
+let pixels;
+
+function getPixels() {
+    do {
+        pixels = prompt('Enter the Number of Pixels!', 'Value must be below 100');
+    } while (pixels > 100);
+    pixels = parseInt(pixels);
 }
+
